5089a44781470b6a74cb8637faacd055fb2a0770
Ben Earle <ben.earle@live.ca>
2018-10-29 11:01:43 -0400

    desired speed calc compiles but is untested

--- desiredSpeedCalculator.cpp
+++ desiredSpeedCalculator.cpp
@@ -11,31 +11,38 @@
 *******************************************************************/
 
 /** include files **/
-#include "desiredSpeedCalculator.h"      // class ActualSpeedCalc
+#include "desiredSpeedCalculator.h"      // class DesiredSpeedCalculator
 #include "message.h"    // class ExternalMessage, InternalMessage
 #include "mainsimu.h"   // MainSimulator::Instance().getParameter( ... )
 
 /** public functions **/
 
 /*******************************************************************
-* Function Name: ActualSpeedCalc
+* Function Name: DesiredSpeedCalculator
 * Description: 
 ********************************************************************/
-ActualSpeedCalc::ActualSpeedCalc( const string &name )
+DesiredSpeedCalculator::DesiredSpeedCalculator( const string &name )
 : Atomic( name )
-, brakeIntensityIn( addInputPort( "brakeIntensityIn" ) )
-, motorDutyCyleIn( addInputPort( "motorDutyCyleIn" ) )
-, actualSpeed( addOutputPort( "actualSpeed" ) )
+, leftRangeIn( addInputPort( "leftRangeIn" ) )
+, centerRangeIn( addInputPort( "centerRangeIn" ) )
+, rightRangeIn( addInputPort( "rightRangeIn" ) )
+, speedIn( addInputPort( "speedIn" ) )
+, odometerIn( addInputPort( "odometerIn" ) )
+, infrastructureIn( addInputPort( "infrastructureIn" ) )
+, desiredSpeedReachedIn( addInputPort( "desiredSpeedReachedIn" ) )
+, desiredSpeedOut( addOutputPort( "desiredSpeedOut" ) )
 {
 	leftRange = 0;
 	centerRange = 0;
 	rightRange = 0;
 	speedLimit = 0;
 	odometer = 0;
-	emergencySpeed = {0, 0};
-	desiredSpeed = {0, 0};
+	emergencySpeed.value = 0;
+	emergencySpeed.distance = 0;
+	desiredSpeed.value = 0;
+	desiredSpeed.distance = 0;
 	speed = 0;
-	nextSign = {NONE, 0, 0};
+	nextSign.type = NONE;
 }
 
 /*******************************************************************
@@ -43,7 +50,7 @@ ActualSpeedCalc::ActualSpeedCalc( const string &name )
 * Description: Resetea la lista
 * Precondition: El tiempo del proximo evento interno es Infinito
 ********************************************************************/
-Model &ActualSpeedCalc::initFunction() {
+Model &DesiredSpeedCalculator::initFunction() {
 	this-> passivate();
 	return *this ;
 }
@@ -52,38 +59,45 @@ Model &ActualSpeedCalc::initFunction() {
 * Function Name: externalFunction
 * Description: 
 ********************************************************************/
-Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
+Model &DesiredSpeedCalculator::externalFunction( const ExternalMessage &msg ) {
 	if( msg.port() == leftRangeIn) {
 		leftRange = float(msg.value());
 		if (this->state() == passive) {
-			passive();
+			passivate();
 		} else {
 			holdIn(active, (msg.time() - lastChange()));
 		}
 	} else if( msg.port() == centerRangeIn) {
 		centerRange = float(msg.value());
 		//float brakingDistance = ((speed < desiredSpeed.value) ? (desiredSpeed.value) : (speed))^2/200;
-		float brakingDistance = (speed)^2/200;
-		if(CurrentCenterRange <= 2) {
-			emergancySpeed = {0,1};
+		float brakingDistance = speed*speed/200;
+		if(centerRange <= 2) {
+			emergencySpeed.value = 0;
+			emergencySpeed.distance = 1;
 			holdIn(active, Time::Zero);
-		} else if (CurrentCenterRange < (brakingDistance + 2)){
-			emergancySpeed = {0, CurrentCenterRange};
+		} else if (centerRange < (brakingDistance + 2)){
+			emergencySpeed.value = 0;
+			emergencySpeed.distance = centerRange;
 			holdIn(active, Time::Zero);
-		} else if (emergancySpeed.value != 0 || emergancySpeed.distance != 0) {
-			emergencySpeed = {0, 0};
+		} else if (emergencySpeed.value != 0 || emergencySpeed.distance != 0) {
+			emergencySpeed.value = 0;
+			emergencySpeed.distance = 0;
 			switch(nextSign.type) {
 				case NONE: 
-					desiredSpeed = {CurrentSpeedLimit, ACCELERATE_DISTANCE};
+					desiredSpeed.value = speedLimit;
+					desiredSpeed.distance = ACCELERATE_DISTANCE;
 					break;
 				case STOP:
-					desiredSpeed = {0, (nextSign.distance - odometer)};
+					desiredSpeed.value = 0;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				case YIELD:
-					desiredSpeed = {YIELD_SPEED, (nextSign.distance - odometer)};
+					desiredSpeed.value = YIELD_SPEED;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				case SPEED:
-					desiredSpeed = {nextSign.value, (nextSign.distance - odometer)};
+					desiredSpeed.value = nextSign.value;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				default:
 					break;
@@ -98,7 +112,7 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 	} else if( msg.port() == rightRangeIn) {
 		rightRange = float(msg.value());
 		if (this->state() == passive) {
-			passive();
+			passivate();
 		} else {
 			holdIn(active, (msg.time() - lastChange()));
 		}
@@ -112,7 +126,7 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 	} else if (msg.port() == odometerIn) {
 		odometer = float(msg.value());
 		if (this->state() == passive) {
-			if(emergancySpeed.value == 0 && emergancySpeed.distance == 0 && odometer = nextSign.distance) {
+			if(emergencySpeed.value == 0 && emergencySpeed.distance == 0 && odometer == nextSign.distance) {
 				holdIn(active, Time::Zero);
 			} else {
 				passivate();
@@ -123,33 +137,38 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 	} else if (msg.port() == infrastructureIn) {
 		if (this->state() == passive) {
 			int temp = float(msg.value());
-			nextSign.type = (temp & 0xC000) >> (30); //top 2 bits are used for type
+			int x = (temp & 0xC000) >> (30);
+			nextSign.type = ((x == 0) ? NONE : ((x == 1) ? STOP : ((x == 2) ? YIELD : SPEED))); //top 2 bits are used for type
 			nextSign.value = ((temp & 0x3E00) >> (25))*5; //next 5 for the value if needed (in km/5hr to preserve bits)
 			nextSign.distance = ((temp& 0x01FF)); //distance in m using the remianing bits
 			nextSign.distance += odometer;
 			switch(nextSign.type) {
 				case NONE: 
-					desiredSpeed = {CurrentSpeedLimit, 10};
+					desiredSpeed.value = speedLimit;
+					desiredSpeed.distance = ACCELERATE_DISTANCE;
 					break;
 				case STOP:
-					desiredSpeed = {0, (nextSign.distance - odometer)}
+					desiredSpeed.value = 0;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				case YIELD:
-					desiredSpeed = {YIELD_SPEED, (nextSign.distance - odometer)}
+					desiredSpeed.value = YIELD_SPEED;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				case SPEED:
-					desiredSpeed = {nextSign.value, (nextSign.distance - odometer)}
+					desiredSpeed.value = nextSign.value;
+					desiredSpeed.distance = (nextSign.distance - odometer);
 					break;
 				default:
 					break;
-            }
+			}
             holdIn(active, Time::Zero);
 		} else {
 			holdIn(active, (msg.time() - lastChange()));
 		}
 	} else if (msg.port() == desiredSpeedReachedIn) {
-		if(emergancySpeed.value != 0 || emergancySpeed.distance != 0) {
-			/* IF the lidar obstical detection is controlling the car, wait until path is clear */
+		if(emergencySpeed.value != 0 || emergencySpeed.distance != 0) {
+			/* IF the lidar obstacle detection is controlling the car, wait until path is clear */
 			passivate();
 		} else {
 			switch(nextSign.type) {
@@ -159,7 +178,8 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 				case STOP:
 					if(leftRange > STOP_DISTANCE && rightRange > STOP_DISTANCE && centerRange > STOP_DISTANCE) {
 						/* The intersection is clear, continue driving */
-						desiredSpeed = {CurrentSpeedLimit, ACCELERATE_DISTANCE};
+						desiredSpeed.value = speedLimit;
+						desiredSpeed.distance = ACCELERATE_DISTANCE;
 						nextSign.type = NONE;
 						holdIn(active, Time::Zero);
 					} else {
@@ -169,11 +189,13 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 				case YIELD:
 					if(leftRange > YIELD_DISTANCE && rightRange > YIELD_DISTANCE && centerRange > YIELD_DISTANCE) {
 						/* The intersection is clear, continue driving */
-						desiredSpeed = {CurrentSpeedLimit, ACCELERATE_DISTANCE};
+						desiredSpeed.value = speedLimit;
+						desiredSpeed.distance = ACCELERATE_DISTANCE;
 						nextSign.type = NONE;
 						holdIn(active, Time::Zero);
-					} else if (desiredSpeed.speed != 0){
-						desiredSpeed = {0, 1}
+					} else if (desiredSpeed.value != 0){
+						desiredSpeed.value = 0;
+						desiredSpeed.distance = 1;
 						holdIn(active, Time::Zero);
 					} else {
 						holdIn(active, WAIT_TIMEOUT);
@@ -195,8 +217,8 @@ Model &ActualSpeedCalc::externalFunction( const ExternalMessage &msg ) {
 * Function Name: internalFunction
 * Description: 
 ********************************************************************/
-Model &ActualSpeedCalc::internalFunction( const InternalMessage & ){
-	if(emergancySpeed.value != 0 || emergancySpeed.distance != 0) {
+Model &DesiredSpeedCalculator::internalFunction( const InternalMessage & ){
+	if(emergencySpeed.value != 0 || emergencySpeed.distance != 0) {
 		/* IF the lidar obstical detection is controlling the car, wait until path is clear */
 		passivate();
 	} else {
@@ -208,7 +230,8 @@ Model &ActualSpeedCalc::internalFunction( const InternalMessage & ){
 			case STOP:
 				if(leftRange > STOP_DISTANCE && rightRange > STOP_DISTANCE && centerRange > STOP_DISTANCE) {
 					/* The intersection is clear, continue driving */
-					desiredSpeed = {CurrentSpeedLimit, ACCELERATE_DISTANCE};
+					desiredSpeed.value = speedLimit;
+					desiredSpeed.distance = ACCELERATE_DISTANCE;
 					nextSign.type = NONE;
 					passivate();
 				} else {
@@ -218,7 +241,8 @@ Model &ActualSpeedCalc::internalFunction( const InternalMessage & ){
 			case YIELD:
 				if(leftRange > YIELD_DISTANCE && rightRange > YIELD_DISTANCE && centerRange > YIELD_DISTANCE) {
 					/* The intersection is clear, continue driving */
-					desiredSpeed = {CurrentSpeedLimit, ACCELERATE_DISTANCE};
+					desiredSpeed.value = speedLimit;
+					desiredSpeed.distance = ACCELERATE_DISTANCE;
 					nextSign.type = NONE;
 					passivate();
 				} else {
@@ -240,11 +264,14 @@ Model &ActualSpeedCalc::internalFunction( const InternalMessage & ){
 * Function Name: outputFunction
 * Description: 
 ********************************************************************/
-Model &ActualSpeedCalc::outputFunction( const InternalMessage &msg) {
-	if(emergancySpeed.value != 0 || emergancySpeed.distance != 0) {
-		sendOutput( msg.time(), desiredSpeedOut, emergencySpeed);
+Model &DesiredSpeedCalculator::outputFunction( const InternalMessage &msg) {
+	int out;
+	if(emergencySpeed.value != 0 || emergencySpeed.distance != 0) {
+		out = ((emergencySpeed.value & 0xF000) << 8) + (emergencySpeed.distance & 0x0FFF);
+		sendOutput( msg.time(), desiredSpeedOut, out);
 	} else {
-		sendOutput( msg.time(), desiredSpeedOut, desiredSpeed);
+		out = ((desiredSpeed.value & 0xF000) << 8) + (desiredSpeed.distance & 0x0FFF);
+		sendOutput( msg.time(), desiredSpeedOut, out);
 	}
 	return *this ;
 }
