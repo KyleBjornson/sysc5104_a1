fb08f99c6c63ff4ca1d4be2e63aa6517d59d48a9
Ben Earle <ben.earle@live.ca>
2018-11-02 21:20:53 -0400

    stop + bat

---
+++ top_test_stop_sign.bat
@@ -0,0 +1,4 @@
+cd /D "C:\eclipse\CD++Builder Example Workspace\sysc5104_a1"
+"C:/eclipse/CD++Builder Example Workspace/sysc5104_a1/simu.exe"  -m"top_test.ma" -e"top_test_signs.ev" -o"top_testOUT_8.out" -l"top_testLOG_8.log" -t"00:20:00:000" 
+rem 
+pause
